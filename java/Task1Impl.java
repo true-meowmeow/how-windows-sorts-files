2f7af95b8fb6e5977846e74d2fcbac5125ebbbd9
meow meow <135vbn@gmail.com>
2020-01-11 04:08:41 +0400

    commit

---
+++ java/Task1Impl.java
@@ -0,0 +1,98 @@
+import java.util.List;
+
+/**
+ * <h1>Задание №1</h1>
+ * Реализуйте интерфейс {@link IStringRowsListSorter}.
+ *
+ * <p>Мы будем обращать внимание в первую очередь на структуру кода и владение стандартными средствами java.</p>
+ */
+public class Task1Impl implements IStringRowsListSorter {
+
+    // ваша реализация должна работать, как singleton. даже при использовании из нескольких потоков.
+    public static final IStringRowsListSorter INSTANCE = new Task1Impl();
+
+
+    @Override
+    public String[] sort(final List<String[]> rows, final int columnIndex) {
+        synchronized ("") {
+            return insertSort(rows, columnIndex);
+        }
+    }
+
+
+    private String[] insertSort(final List<String[]> rows, final int columnIndex) {
+        String[] array = rows.get(columnIndex);
+
+        for (int i = 0; i < rows.get(columnIndex).length; i++) {
+            String newElement = rows.get(columnIndex)[i];
+            int location = i - 1;
+            while (location >= 0 && logic(array[location], newElement) > 0) {
+                array[location + 1] = array[location];
+                location--;
+            }
+            array[location + 1] = newElement;
+        }
+        return array;
+    }
+
+
+    private int logic(String s1, String s2) {
+        if (s1 == null) return -1; //если null -> двигаем влево
+        if (s2 == null) return 1;
+        if (s1.equals("")) return -1; //если нет знака -> двигаем влево, но не так же далеко как null, поэтому они в разных условиях
+        if (s2.equals("")) return 1;
+        if (s1.equals(s2)) return 0; //если они равны, то нет смысла что-то делать
+
+
+        int iteration = 0;
+        int i;
+        while (((i = compare(s1, s2, iteration)) == 0)) {
+            iteration++;
+        }
+        return i;
+    }
+
+
+    private int compare(String s1, String s2, int iteration) {
+
+        s1 = substring(s1, iteration);
+        s2 = substring(s2, iteration);
+
+        try {
+            return Integer.compare(Integer.parseInt(s1), Integer.parseInt(s2));
+        } catch (Exception ignored) {
+            return s1.compareTo(s2);
+        }
+    }
+
+    //получает сравниваемую часть по слову и по её номеру
+    private StringBuilder stringBuilder = new StringBuilder();
+    private String substring(String s, int iteration) {
+        boolean firstCharIsNumber = isNumber(s.charAt(0));
+        stringBuilder.setLength(0);
+        int i = 0;
+        for (int j = 0; j < s.length(); j++) {
+            if (isNumber(s.charAt(j)) && !firstCharIsNumber) {
+                firstCharIsNumber = true;
+                i++;
+            } else if (!isNumber(s.charAt(j)) && firstCharIsNumber) {
+                firstCharIsNumber = false;
+                i++;
+            }
+            if (i == iteration) {
+                stringBuilder.append(s.charAt(j));
+            }
+            if (i > iteration) return stringBuilder.toString();
+        }
+        return stringBuilder.toString();
+    }
+
+    private boolean isNumber(char c) {
+        try {
+            Integer.parseInt(String.valueOf(c));
+            return true;
+        } catch (Exception ignored) {
+            return false;
+        }
+    }
+}
---
+++ java/Test.java
@@ -0,0 +1,39 @@
+import org.junit.Before;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Test {
+
+    private List<String[]> list;
+    private int column;
+
+    @Before
+    public void before() {
+        list = new ArrayList<>();
+        column = 0;
+    }
+
+    @org.junit.Test
+    public void test() {
+
+        list.add(new String[]{"111хх25dddd03", "111хх25dddd034s", "111хх25dddd03", null, "юю", "12341fdas", "0", "22", " ", "23", "sa", "123zxfde143", "12341fda", " ", null, "", "sf234", "fsdfs23"}); // <-- unsorted
+        //list.add(new String[]{null, null, "", " ", " ", "0", "22", "23", "111хх25dddd03", "111хх25dddd03", "111хх25dddd034s", "123zxfde143", "12341fda", "12341fdas", "fsdfs23", "sa", "sf234", "юю"}); // <-- sorted
+        output(list, column);
+
+        list.set(column, Task1Impl.INSTANCE.sort(list, column));
+        output(list, column);
+    }
+
+    public static void output(List<String[]> list, int column) {
+        System.out.println("\n" + Arrays.toString(list.get(column)));
+    }
+
+
+
+
+
+
+}
